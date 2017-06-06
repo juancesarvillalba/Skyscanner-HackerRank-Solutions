6341f58bfd1a36a88dc6dbd9e8463562bf3b4c15
Simone Vittori <hello@simonewebdesign.it>
2017-06-06 09:06:30 +0100

    Add test scaffold

---
+++ test.js
@@ -0,0 +1,23 @@
+// the test framework
+let assert = require('assert')
+
+// the input
+let stdin = ``
+// the expectation
+let expected = ``
+// the input
+let stdin2 = ``
+// the expectation
+let expected2 = ``
+// the test
+it('meets the expectation', () =>
+    assert.equal(main(stdin), expected))
+
+it('meets the expectation 2', () =>
+    assert.equal(main(stdin2), expected2))
+
+
+
+// the code
+function main(input) {
+}
